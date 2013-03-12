14ee05a1f945067c7d5b07fc6d6f331dfd349155
Costa Halicea <costa@halicea.com>
2013-03-12 14:44:50 +0100

    fix on the delete file plugin

--- delete_file.py
+++ delete_file.py
@@ -2,12 +2,14 @@ import os
 from hal_configurator.lib.command_base import OperationBase, ArgumentDescriptor
 class DeleteFile(OperationBase):
   code = "delete_file"
-  def __init__(self):
-    pass
-  def get_arg_descriptors(self):
+  def __init__(self, *args, **kwargs):
+    super(DeleteFile, self).__init__(*args, **kwargs)
+
+  @classmethod
+  def get_arg_descriptors(cls):
     return [
-            ArgumentDescriptor("FilePath", "The absolute file path of the file to be deleted", "text", os.path.exists)
-            ]
+      ArgumentDescriptor("FilePath", "The absolute file path of the file to be deleted", "text", os.path.exists)
+    ]
   def run(self, **kwargs):
     os.remove(kwargs["FilePath"])
 
