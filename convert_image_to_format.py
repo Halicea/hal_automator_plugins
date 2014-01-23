3f3d71fead78f789f187daebfda6450bd09e332c
Costa Halicea <costa@halicea.com>
2014-01-23 15:08:00 +0100

    no message

--- convert_image_to_format.py
+++ convert_image_to_format.py
@@ -13,7 +13,7 @@ class ConvertImageToFormat(OperationBase):
     super(ConvertImageToFormat, self).__init__(*args, **kwargs)
     self.result = ''
 
-  def set_args(self, ImageResource, Destination, Format):
+  def set_args(self, ImageResource, Destination, Format, Resolution):
     """
     :param ConfigPath:
     :type ConfigPath: str
@@ -21,15 +21,40 @@ class ConvertImageToFormat(OperationBase):
     self.kwargs["ImageResource"] = self.img_res = ImageResource
     self.kwargs["Destination"] = self.dest = Destination
     self.kwargs["Format"] = self.format = Format
+    self.kwargs["Resolution"] = self.resolution = Resolution
+
+  def get_resolution_set(self):
+    print "Resolution:",self.resolution
+
+    if 'x' in self.resolution:
+      return tuple([int(x) for x in self.resolution.split('x')])
+    elif 'X' in self.resolution:
+      return tuple([int(x.strip()) for x in self.resolution.split('X')])
+    elif ',' in self.resolution:
+      return tuple([int(x.strip()) for x in self.resolution.split(',')])
+    else:
+      raise Exception('Invalid resolution: %s'%self.resolution)
 
   def run(self):
     is_valid, errors = self.validate_args()
     if is_valid:
       self.img_res = self.value_substitutor.substitute(self.img_res)
+      if self.img_res.startswith('file://'):
+        self.img_res = self.img_res[7:]
+
       self.dest = self.value_substitutor.substitute(self.dest)
+
+      self.img_res = self.img_res.replace('./', '')
       self.format = self.value_substitutor.substitute(self.format)
+
+      if self.resolution:
+        self.resolution = self.value_substitutor.substitute(self.resolution)
+      self.log.write('\n'.join([self.img_res, self.dest, self.format, self.resolution]))
       img = Image.open(self.img_res);
-      img.save(self.dest, self.format);
+      if self.resolution:
+        print self.get_resolution_set()
+        img = img.resize(self.get_resolution_set(), Image.ANTIALIAS)
+      img.save(self.dest, self.format)
     else:
       raise InvalidCommandArgumentsError(str(errors))
 
@@ -38,7 +63,8 @@ class ConvertImageToFormat(OperationBase):
     return [
       ArgumentDescriptor("ImageResource", "External Configuration Directory", "text"),
       ArgumentDescriptor("Destination", "External Configuration Directory", "text"),
-      ArgumentDescriptor("Format", "External Configuration Directory", "text")
+      ArgumentDescriptor("Format", "External Configuration Directory", "text"),
+      ArgumentDescriptor("Resolution", "The required resolution", "text", default_value_lambda=lambda x:None)
     ]
   @classmethod
   def get_name(cls):
