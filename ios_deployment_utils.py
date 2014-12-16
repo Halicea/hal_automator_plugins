27738635bacb8772fb5921dab9e4b8f25c0cf5a7
Halicea <costa@halicea.com>
2014-12-16 23:08:41 +0100

    the certificate and provisioning profile setup is working ok now

--- ios_deployment_utils.py
+++ ios_deployment_utils.py
@@ -1,13 +1,16 @@
 from os.path import expanduser
-import argparse
+import re
+import datetime
 import os
 import shutil
 import subprocess
 import tempfile
-import sys
 profiles_dir = expanduser("~/Library/MobileDevice/Provisioning Profiles")
 default_keychain = expanduser('~/Library/Keychains/login.keychain')
+
+
 class CommandArgsProxy(object):
+
   def __init__(self, **kwargs):
     self.command = kwargs.has_key('command') and kwargs['command'] or None
     self.command_type = kwargs.has_key('command_type') and kwargs['command_type'] or None
@@ -15,12 +18,15 @@ class CommandArgsProxy(object):
     self.match = kwargs.has_key('match') and kwargs['match'] or ''
     self.keychain = kwargs.has_key('keychain') and kwargs['keychain'] or default_keychain
 
+
 def get_info(path, args=None):
+
   def parse_uuid(prof_str):
     ind = prof_str.index('<key>UUID</key>\n')+len('<key>UUID</key>\n')
     l = prof_str.index('<string>', ind)+len('<string>')
     r = prof_str.index('</string>', ind)
     return prof_str[l:r]
+
   def parse_name(prof_str):
     try:
       ind = prof_str.index('<key>AppIDName</key>\n')+len('<key>AppIDName</key>\n')
@@ -33,41 +39,91 @@ def get_info(path, args=None):
       l = prof_str.index('<string>', ind)+len('<string>')
       r = prof_str.index('</string>', ind)
       return prof_str[l:r]
+
   def parse_target(prof_str):
-    ind = prof_str.index('<key>application-identifier</key>\n')+len('<key>application-identifier</key>\n')
+    key = '<key>application-identifier</key>\n'
+    ind = prof_str.index(key)+len(key)
     l = prof_str.index('<string>', ind)+len('<string>')
     r = prof_str.index('</string>', ind)
     return prof_str[l:r]
+
+  def parse_type(prof_str):
+    key = '<key>aps-environment</key>\n'
+    try:
+        ind = prof_str.index(key)+len(key)
+        l = prof_str.index('<string>', ind)+len('<string>')
+        r = prof_str.index('</string>', ind)
+        return prof_str[l:r]
+    except:
+      if '<key>DeveloperCertificates</key>' in prof_str:
+        return 'development'
+      else:
+        raise
+
+  def parse_profile_name(prof_str):
+    key = '<key>Name</key>'
+    ind = prof_str.index(key)+len(key)
+    l = prof_str.index('<string>', ind)+len('<string>')
+    r = prof_str.index('</string>', ind)
+    return prof_str[l:r]
+
+  def parse_exp_date(prof_str):
+    key = '<key>ExpirationDate</key>'
+    ind = prof_str.index(key)+len(key)
+    l = prof_str.index('<date>', ind)+len('<date>')
+    r = prof_str.index('</date>', ind)
+    s = prof_str[l:r]
+    d = datetime.datetime(*map(int, re.split('[^\d]', s)[:-1]))
+    return d
+
   with open(path, 'r') as f:
     t = f.read()
     res = {}
-    res['uuid']=parse_uuid(t)
-    res['name']=parse_name(t)
-    res['target']=parse_target(t)
+    res['path'] = os.path.abspath(path)
+    res['uuid'] = parse_uuid(t)
+    res['name'] = parse_name(t)
+    res['profile_name'] = parse_profile_name(t)
+    res['expiration_date'] = parse_exp_date(t)
+    res['target'] = parse_target(t)
+    res['type'] = parse_type(t)
     return res
+
 
 def add_profile(path, args=None):
   info = get_info(path)
-  shutil.copy(path, os.path.join(profiles_dir, '%s.mobileprovision'%info['uuid']))
+  j = os.path.join
+  shutil.copy(path, j(profiles_dir, '%s.mobileprovision' % info['uuid']))
+
 
 def rm_profile(path, args=None):
   info = get_info(path)
-  os.remove(os.path.join(profiles_dir, '%s.mobileprovision'%info['uuid']))
+  os.remove(os.path.join(profiles_dir, '%s.mobileprovision' % info['uuid']))
 
-def ls_profiles(path=None, args=None):
+def ls_profiles(path=None, target=None, profile_type='production'):
   infos = []
-  for p in os.listdir(profiles_dir):
+  _pdir = path
+  if path is None:
+    _pdir = profiles_dir
+  for p in os.listdir(_pdir):
     if p.endswith('.mobileprovision'):
-      pfile = os.path.join(profiles_dir, p)
+      pfile = os.path.join(_pdir, p)
       info = get_info(pfile)
-      infos.append(info)
-  counter=1
-  infos = sorted(infos, key=lambda x: x['name'], reverse=False)
+      if target:
+        if target in info['target']:
+          if profile_type:
+            if 'type' in info and info['type'] == profile_type:
+              infos.append(info)
+          else:
+            infos.append(info)
+      else:
+        infos.append(info)
+  counter = 1
+  infos = sorted(infos, key=lambda x: x['expiration_date'], reverse=False)
   for info in infos:
     print '*'*20
-    print str(counter)+".", info['name'], info['target'], info['uuid']
+    print str(counter)+".", info['name'], info['target'], info['uuid'], info["type"]
     print '*'*20
-    counter+=1
+    counter += 1
   return infos
 
 def call(cmd):
@@ -77,23 +133,27 @@ def call(cmd):
   with tempfile.TemporaryFile() as output_f:
     with tempfile.TemporaryFile() as error_f:
       p = subprocess.Popen(cmd, stderr=error_f, stdout=output_f,
-                         close_fds=True, env=os.environ)
+                           close_fds=True, env=os.environ)
       code = p.wait()
       output_f.seek(0)
       error_f.seek(0)
       error = error_f.read().split('\n')
       output = output_f.read().split('\n')
-  if code>0:
-    raise Exception('\n'.join(output)+'\n'.join(error)+'\nShell process exited with error code %s\n cmd: %s'%(code, cmd))
+  if code > 0:
+    raise Exception('\n'.join(output)+'\n'.join(error) +
+                    '\nShell process exited with error code %s' % code +
+                    '\n cmd: %s' % cmd)
   return '\n'.join(output)
 
 def add_cert(path, args=None):
-  cmd='/usr/bin/security import %s -k %s -P %s -T  -A /usr/bin/codesign'%(path, args.keychain, args.passwd)
+  cmd = '/usr/bin/security import %s -k %s -P %s -T  -A /usr/bin/codesign'
+  cmd = cmd % (path, args.keychain, args.passwd)
   print cmd
   call(cmd.split(' '))
 
 def ls_certs(args):
-  command = "/usr/bin/security dump-keychain {} |grep \"labl\"".format(args.keychain)
+  command = "/usr/bin/security dump-keychain {} |grep \"labl\""
+  command = command.format(args.keychain)
   cmd = [x for x in command.split(' ') if x]
   output = call(cmd).split('\n')
   prefix = "\"labl\"<blob>=\""
@@ -105,9 +165,8 @@ def ls_certs(args):
   return result
 
 def get_cert_sha1(common_name, args):
-  #call(["/usr/bin/security", "find-certificate", "-a", "-Z", "-c", "\"iPhone Distribution: Entreprises Kastoff Inc.\"", "/Users/halicea/Library/Keychains/login.keychain"])
-    #/usr/bin/security find-certificate -a -Z -c "iPhone Distribution: Entreprises Kastoff Inc." /Users/halicea/Library/Keychains/login.keychain
-  cmd = ['/usr/bin/security','find-certificate','-a','-Z','-c',common_name, args.keychain]
+  cmd = ['/usr/bin/security', 'find-certificate', '-a', '-Z', '-c',
+         common_name, args.keychain]
   output = call(cmd).split('\n')
   prefix = "SHA-1 hash:"
   result = []
@@ -119,41 +178,22 @@ def get_cert_sha1(common_name, args):
 def rm_cert(cert_name=None, sha1=None, args=None):
   cmd = None
   if cert_name:
-    cmd = ['/usr/bin/security', 'delete-certificate', '-c', '%s'%cert_name, args.keychain]
+    cmd = ['/usr/bin/security', 'delete-certificate', '-c', cert_name, args.keychain]
   if sha1:
     cmd = ['/usr/bin/security', 'delete-certificate', '-Z', sha1, args.keychain]
   if cmd:
     print cmd
     call(cmd)
 
-command_dict={
-  "cert":{
-    "add":add_cert,
-    "rm":rm_cert,
-    "list":ls_certs
-  },
-  "profile":{
-    "add":add_profile,
-    "rm":rm_profile,
-    "list": ls_profiles
-  }
+command_dict = {
+    'cert': {
+        'add': add_cert,
+        'rm': rm_cert,
+        'list': ls_certs
+    },
+    'profile': {
+        'add': add_profile,
+        'rm': rm_profile,
+        'list': ls_profiles
+    }
 }
-
-if __name__=='__main__':
-  #"Apple Production IOS Push Services:"
-  #"Apple Developer IOS Push Services:"
-  parser = argparse.ArgumentParser(prog="iOS Provisioning Profiles and Keychain Utility")
-  parser.add_argument('command', choices=['add', 'rm', 'list'])
-  parser.add_argument('command_type', choices=['cert', 'profile', 'list'])
-  parser.add_argument('--passwd', '-p', help='Password of the Certificate file')
-  parser.add_argument('--match', '-m', help='Match for certificate', default='')
-  parser.add_argument('--keychain', '-k', help='Keychain', default=default_keychain)
-  parser.add_argument('--file')
-  args = parser.parse_args(sys.argv[1:])
-
-  command_dict[args.command_type][args.command](args.file, args)
-#security find-certificate -a -p -c "com.turn-page.CampCalifornia" /Users/kostamihajlov/Library/Keychains/login.keychain"
-#security find-certificate -a -p -Z -c "com.turn-page.CampCalifornia" /Users/kostamihajlov/Library/Keychains/login.keychain"
-#"security delete-certificate -a -c "Apple Development IOS Push Services: com.turn-page.CampCalifornia" /Users/kostamihajlov/Library/Keychains/login.keychain"
-#security delete-certificate -Z E6A44C7CEF6ED79709107C5EF9AE88986F01AFC8 /Users/kostamihajlov/Library/Keychains/login.keychain
-  #install_profile(sys.argv[1])
--- ios_utils.py
+++ ios_utils.py
@@ -1,25 +1,34 @@
-from hal_configurator.lib.command_base import OperationBase, ArgumentDescriptor
-from ios_deployment_utils import add_cert, rm_cert, ls_certs, get_cert_sha1
-from ios_deployment_utils import add_profile, rm_profile, CommandArgsProxy
-from replace_from_url import ReplaceFromUrl
 import os
+from hal_configurator.lib.command_base import OperationBase, ArgumentDescriptor
+from ios_deployment_utils import(
+    add_cert, rm_cert, ls_certs, get_cert_sha1,
+    add_profile, rm_profile, ls_profiles,
+    CommandArgsProxy)
 
-__author__ = 'Costa Halicea'
+from replace_from_url import ReplaceFromUrl
 
 class InstallIOSCertificate(OperationBase):
-  def __init__(self,*args, **kwargs):
+
+  def __init__(self, *args, **kwargs):
     super(InstallIOSCertificate, self).__init__(*args, **kwargs)
     self.result = ''
     self.downloader = ReplaceFromUrl(*args, **kwargs)
+
   @classmethod
   def get_arg_descriptors(cls):
     return [
-            ArgumentDescriptor("Resource", "The Developer Certificate resource", "text"),
-            ArgumentDescriptor("Password", "Password to unlock the certificate", "text")
-           ]
+        ArgumentDescriptor(
+            "Resource",
+            "The Developer Certificate resource",
+            "text"),
+        ArgumentDescriptor(
+            "Password",
+            "Password to unlock the certificate",
+            "text")]
+
   def set_args(self, Resource, Password):
-    self.kwargs["Resource"]=self.cert = Resource
-    self.kwargs["Password"]=self.password = Password
+    self.kwargs["Resource"] = self.cert = Resource
+    self.kwargs["Password"] = self.password = Password
 
   def run(self):
     self.downloader.set_args(self.cert, '/tmp/temp.p12')
@@ -27,29 +36,39 @@ class InstallIOSCertificate(OperationBase):
     add_cert('/tmp/temp.p12', CommandArgsProxy(passwd=self.password))
     os.unlink('/tmp/temp.p12')
 
+
 class RemoveIOSCertificates(OperationBase):
+
   '''
-  Removes a certificate from the current system based on the match string providede
+  Removes a certificate from the current system
+  based on the match string providede
   '''
-  def __init__(self,*args, **kwargs):
+  def __init__(self, *args, **kwargs):
     super(RemoveIOSCertificates, self).__init__(*args, **kwargs)
     self.result = ''
     home = os.path.expanduser("~")
-    self.profiles_dir = os.path.join(home, 'Library/MobileDevice/Provisioning Profiles/')
+    j = os.path.join
+    self.profiles_dir = j(home, 'Library/MobileDevice/Provisioning Profiles/')
+
   @classmethod
   def get_arg_descriptors(cls):
     return [
-            ArgumentDescriptor("SearchString", "The Certificates match pattern", "text"),
-           ]
+        ArgumentDescriptor(
+            "SearchString",
+            "The Certificates match pattern",
+            "text")]
+
   def set_args(self, SearchString):
-    self.kwargs["SearchString"]=self.search_string = SearchString
+    self.kwargs["SearchString"] = self.search_string = SearchString
 
   def run(self):
-    self.kwargs["SearchString"]=self.search_string = self.value_substitutor.substitute(self.search_string)
+    s = self.value_substitutor.substitute
+    self.kwargs["SearchString"] = self.search_string = s(self.search_string)
     items = ls_certs(CommandArgsProxy())
     certs_to_delete = []
     for cert_name in items:
-      print "Found Cert:{} matching with:{}".format(cert_name, self.search_string)
+      msg = "Found Cert:{} matching with:{}"
+      print msg.format(cert_name, self.search_string)
       if self.search_string in cert_name:
         certs_to_delete.append(cert_name)
     args = CommandArgsProxy()
@@ -59,22 +78,29 @@ class RemoveIOSCertificates(OperationBase):
       for sha in sha1s:
           rm_cert(sha1=sha, args=args)
 
+
 class ListIOSCertificates(OperationBase):
+
   '''
   Lists all the certificates it can match
   '''
-  def __init__(self,*args, **kwargs):
+  def __init__(self, *args, **kwargs):
     super(ListIOSCertificates, self).__init__(*args, **kwargs)
     self.result = ''
     home = os.path.expanduser("~")
-    self.profiles_dir = os.path.join(home, 'Library/MobileDevice/Provisioning Profiles/')
+    j = os.path.join
+    self.profiles_dir = j(home, 'Library/MobileDevice/Provisioning Profiles/')
+
   @classmethod
   def get_arg_descriptors(cls):
     return [
-            ArgumentDescriptor("SearchString", "The Certificates match pattern", "text"),
-           ]
+        ArgumentDescriptor(
+            "SearchString",
+            "The Certificates match pattern",
+            "text")]
+
   def set_args(self, SearchString):
-    self.kwargs["SearchString"]=self.search_string = SearchString
+    self.kwargs["SearchString"] = self.search_string = SearchString
 
   def run(self):
     self.search_string = self.value_substitutor.substitute(self.search_string)
@@ -85,43 +111,79 @@ class ListIOSCertificates(OperationBase):
 
 
 class AddIOSProvisioningProfile(OperationBase):
-  def __init__(self,*args, **kwargs):
+
+  def __init__(self, *args, **kwargs):
     super(AddIOSProvisioningProfile, self).__init__(*args, **kwargs)
     self.provistioning_profile = None
     self.downloader = ReplaceFromUrl(*args, **kwargs)
+
   @classmethod
   def get_arg_descriptors(cls):
     return [
-            ArgumentDescriptor("Resource", "The provisioning profile resource", "text")
-           ]
+        ArgumentDescriptor(
+            "Resource",
+            "The provisioning profile resource",
+            "text")]
+
   def set_args(self, Resource):
-    self.kwargs["Resource"]=self.provistioning_profile = Resource
+    self.kwargs["Resource"] = self.provistioning_profile = Resource
 
   def run(self):
-    self.downloader.set_args(self.provistioning_profile, '/tmp/temp.mobileprovision')
-    self.downloader.run()
-    add_profile('/tmp/temp.mobileprovision', CommandArgsProxy())
-    os.unlink('/tmp/temp.mobileprovision')
-    self.result =  True
+    profile_item = self.provistioning_profile
+
+    def is_file():
+        downloader_prefixes = ['file://', 'http://', 'ftp://', 'https://']
+        for dp in downloader_prefixes:
+            if profile_item.startswith(dp):
+                return True
+        return False
+    if is_file():
+        self.downloader.set_args(profile_item, '/tmp/temp.mobileprovision')
+        self.downloader.run()
+        add_profile('/tmp/temp.mobileprovision', CommandArgsProxy())
+        os.unlink('/tmp/temp.mobileprovision')
+        self.result = True
+    else:
+        path, target, profile_type = profile_item.split("*")
+        profile_infos = ls_profiles(path, target, profile_type)
+        for info in profile_infos:
+            self.downloader.set_args(
+                'file://'+info['path'],
+                '/tmp/temp.mobileprovision')
+            self.downloader.run()
+            add_profile('/tmp/temp.mobileprovision', CommandArgsProxy())
+            os.unlink('/tmp/temp.mobileprovision')
+
 
 class RemoveIOSProvisioningProfile(OperationBase):
-  def __init__(self,*args, **kwargs):
+
+  def __init__(self, *args, **kwargs):
     super(RemoveIOSProvisioningProfile, self).__init__(*args, **kwargs)
     self.downloader = ReplaceFromUrl(*args, **kwargs)
+
   @classmethod
   def get_arg_descriptors(cls):
     return [
-            ArgumentDescriptor("Resource", "The provistioning profile resource", "text")
-           ]
-  def set_args(self, Resource):
+        ArgumentDescriptor(
+            "Resource",
+            "The provistioning profile resource",
+            "text")]
 
-    self.kwargs["Resource"]=self.provistioning_profile = Resource
+  def set_args(self, Resource):
+    self.kwargs["Resource"] = self.provistioning_profile = Resource
 
   def run(self):
-    self.downloader.set_args(self.provistioning_profile, '/tmp/temp.mobileprovision')
+    self.downloader.set_args(
+        self.provistioning_profile,
+        '/tmp/temp.mobileprovision')
     self.downloader.run()
     rm_profile('/tmp/temp.mobileprovision', CommandArgsProxy())
     os.unlink('/tmp/temp.mobileprovision')
     self.result = True
 
-__plugins__ = [InstallIOSCertificate, RemoveIOSCertificates, ListIOSCertificates, AddIOSProvisioningProfile, RemoveIOSProvisioningProfile]
+__plugins__ = [
+    InstallIOSCertificate,
+    RemoveIOSCertificates,
+    ListIOSCertificates,
+    AddIOSProvisioningProfile,
+    RemoveIOSProvisioningProfile]
